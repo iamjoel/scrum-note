574384fdbed32a5a7aec891051af4bdad477eaef
joel <iamjoel007@gmail.com>
2020-02-14 22:54:26 +0800

    enchance

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,8 +7,7 @@ module.exports = {
   themeConfig: {
     // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%BC%E8%88%AA%E6%A0%8F
     nav: [
-      // {text: 'GitHub', link: 'https://github.com/iamjoel'},
-      // {text: '简书', link: 'https://www.jianshu.com/u/EhUmA3'}
+      {text: 'GitHub', link: 'https://github.com/iamjoel/scrum-note'},
     ],
     // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E4%BE%A7%E8%BE%B9%E6%A0%8F
     sidebar: [
